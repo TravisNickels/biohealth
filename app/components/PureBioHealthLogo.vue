0d4e77ed77c297d86c8840c4ef4f9ac0a8f6ae96
Travis Nickels <nickels.travis@gmail.com>
2025-12-28 21:23:41 -0800

    Add Components

---
+++ app/components/PureBioHealthLogo.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 4663 1167"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xml:space="preserve"
+    xmlns:serif="http://www.serif.com/"
+    style="fill-rule: evenodd; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 1.5"
+  >
+    <text
+      x="293.696px"
+      y="728.267px"
+      style="
+        font-family:
+          HighTowerText-Reg,
+          High Tower Text,
+          serif;
+        font-size: 1250px;
+        fill: #ffaed5;
+      "
+    >
+      µ
+    </text>
+    <circle cx="582.088" cy="582.088" r="556.046" style="fill: none; stroke: #ffaed5; stroke-width: 52.08px" />
+    <g>
+      <text x="1284.54px" y="811.591px" style="font-family: Poppins-Regular, Poppins; font-size: 625.001px; fill: #ffaed5">Pµre</text>
+      <text x="2804.51px" y="811.591px" style="font-family: Poppins-Regular, Poppins; font-size: 390.627px; fill: #ebfcff">BioHealth</text>
+    </g>
+  </svg>
+</template>
